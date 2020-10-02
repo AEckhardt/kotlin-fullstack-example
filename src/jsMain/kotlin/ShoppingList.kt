e146eb1b361d8c02f2d08972ea81bb90b163506a
AEckhardt <eckeannas@gmail.com>
2020-10-02 12:08:06 +0200

    Fehler in App

---
+++ src/jsMain/kotlin/ShoppingList.kt
@@ -0,0 +1,39 @@
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.*
+
+external interface ShoppingListProps : RProps {
+    var currentShoppingList: List<ShoppingListItem>
+    var onClickItem: (ShoppingListItem) -> Unit
+}
+
+class ShoppingList: RComponent<ShoppingListProps, RState>() {
+    override fun RBuilder.render() {
+        table {
+            thead {
+                tr{
+                    th{+"Priority"}
+                    th{+"Item"}
+                }
+                }
+            tbody {
+            props.currentShoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+                tr {
+                    key = item.toString()
+                    attrs.onClickFunction = {
+                        props.onClickItem(item)
+                    }
+                    td{+"${item.priority}"}
+                    td{+item.desc}
+                }
+            }
+            }
+        }
+    }
+}
+
+fun RBuilder.shoppingListComponent(handler: ShoppingListProps.() -> Unit): ReactElement {
+    return child(ShoppingList::class) {
+        this.attrs(handler)
+    }
+}
--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,71 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.reactivestreams.KMongo
+import com.mongodb.ConnectionString
+
+
+
 fun main() {
-    println("Hello, JVM!")
+    val port = System.getenv("PORT")?.toInt() ?: 9090
+    val client = KMongo.createClient().coroutine
+    val database = client.getDatabase("shoppingList")
+    val collection = database.getCollection<ShoppingListItem>()
+
+
+    embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+        routing {
+            get("/"){
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/"){
+                resources("")
+            }
+            route(ShoppingListItem.path){
+                get {
+                    call.respond(collection.find().toList())
+                }
+                post {
+                    collection.insertOne(call.receive<ShoppingListItem>())
+                    call.respond(HttpStatusCode.OK)
+                }
+                route("/{id}"){
+                    delete(){
+                        val id = call.parameters["id"]?.toInt()?:error("Invalid delete request")
+                        collection.deleteOne(ShoppingListItem::id eq id)
+                        call.respond(HttpStatusCode.OK)
+                    }
+                    get() {
+                        val id = call.parameters["id"]?.toInt()?:error("Invalid get request")
+                        call.respond(collection.find(ShoppingListItem::id eq id))
+                    }
+                }
+            }
+        }
+    }.start(wait = true)
+
 }
